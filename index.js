473a276be3d29bb45ef6cf1d4ae6e75bd4f31d4b
Joshua Burke <burke.joshua.james@gmail.com>
2020-02-11 13:48:54 -0500

    Demonstrate a list selection after an input

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 import input from '@inquirer/input';
+import select from '@inquirer/select';
 
 async function main() {
-  const name = {}
+  const person = {}
   const answer = await input({ message: 'Enter your FIRST name' })
-    .then(async (firstName) => {
-      name.first = firstName;
+    .then((firstName) => {
+      person.first = firstName;
+      return input({ message: 'Enter your MIDDLE name' });
+    })
+    .then((middleName) => {
+      person.middle = middleName;
       return input({ message: 'Enter your LAST name' });
     })
     .then((lastName) => {
-      name.last = lastName;
-      console.log(`Your full name is ${name.first}, ${name.last}`);
-      return name;
+      person.last = lastName;
+      return select({
+        message: 'What is your favorite color',
+        choices: [
+          { name: 'blue', value: 'blue' },
+          { name: 'red', value: 'red' },
+          { name: 'yellow', value: 'yellow', disabled: true }
+        ],
+      });
+    })
+    .then((favoriteColor) => {
+      person.color = favoriteColor;
+      return person;
     });
+  console.log(`Your full name is ${person.first}, ${person.last}`);
+  console.log(`Your color is ${person.color}`);
   console.log({ answer });
   return answer;
 }
